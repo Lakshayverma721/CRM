b4b0c795a791697bd62f5768253bd763ea640c13
lakshayverma819 <lakshayverma819@gmail.com>
2025-03-20 11:01:49 +0530

    test commit

---
+++ CRMProject-main_CRMProject-main/src/test/java/springboot/springdatajpa/crmproject2/CrMproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.springdatajpa.crmproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrMproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
